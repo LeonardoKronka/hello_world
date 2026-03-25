6b884ede75315e49551aa36476c0959c4cb3ae3b
LeonardoKronka <leonardo.k.rodrigues@aluno.senai.br>
2026-03-25 09:14:56 -0300

    main-widget.dart

--- lib/main.dart
+++ lib/main.dart
@@ -11,69 +11,37 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(
-            "Atividade 1 - Container + Column + Row",
-          ),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                width: 200,
-                height: 100,
-                color: const Color.fromARGB(255, 8, 0, 255),
-                child: Center(
-                  child: Text(
-                    "Caixa 1",
-                    style: TextStyle(
-                      color: const Color.fromARGB(230, 255, 255, 255),
-                    ),
-                  ),
-                ),
-              ),
+      home: ContadorTela(),
+    );
+  }
+}
 
-              SizedBox(height: 20),
+class ContadorTela extends StatefulWidget {
+  const ContadorTela({super.key});
 
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    width: 80,
-                    height: 80,
-                    color: const Color.fromARGB(255, 255, 0, 0),
-                    child: Center(
-                      child: Text(
-                        "",
-                        style: TextStyle(
-                          color: const Color.fromARGB(230, 255, 255, 255),
-                        ),
-                      ),
-                    ),
-                  ),
+  @override 
+  _ContadorTelaState createState() => _ContadorTelaState();
+}
 
-                  SizedBox(width: 20),
+class _ContadorTelaState extends State<ContadorTela> {
+  int contador = 0;
 
-                  Container(
-                    width: 80,
-                    height: 80,
-                    color: const Color.fromARGB(255, 20, 122, 30),
-                    child: Center(
-                      child: Text(
-                        "",
-                        style: TextStyle(
-                          color: const Color.fromARGB(230, 255, 255, 255),
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ),
+  void incrementar() {
+    setState((){
+      contador++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Exemplo setState")),
+      body: Center(
+        child: Text("Contador: $contador", style: TextStyle(fontSize: 30)),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: incrementar,
+        child: Icon(Icons.add),
       ),
     );
   }
