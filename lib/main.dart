9a808865adca9fcdbd8804b7bbaf9cca68ba5756
LeonardoKronka <leonardo.k.rodrigues@aluno.senai.br>
2026-03-25 11:15:27 -0300

    Widgets - atividades

--- lib/main.dart
+++ lib/main.dart
@@ -10,25 +10,31 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: ContadorTela(),
-    );
+    return MaterialApp(home: ContadorCurtidas());
   }
 }
 
-class ContadorTela extends StatefulWidget {
-  const ContadorTela({super.key});
+class ContadorCurtidas extends StatefulWidget {
+  const ContadorCurtidas({super.key});
 
-  @override 
-  _ContadorTelaState createState() => _ContadorTelaState();
+  @override
+  _ContadorCurtidasState createState() => _ContadorCurtidasState();
 }
 
-class _ContadorTelaState extends State<ContadorTela> {
+class _ContadorCurtidasState extends State<ContadorCurtidas> {
   int contador = 0;
 
   void incrementar() {
-    setState((){
+    setState(() {
       contador++;
+    });
+  }
+
+  void excluir() {
+    setState(() {
+      if (contador > 0) {
+        contador--;
+      }
     });
   }
 
@@ -37,11 +43,38 @@ class _ContadorTelaState extends State<ContadorTela> {
     return Scaffold(
       appBar: AppBar(title: Text("Exemplo setState")),
       body: Center(
-        child: Text("Contador: $contador", style: TextStyle(fontSize: 30)),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: incrementar,
-        child: Icon(Icons.add),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.favorite,
+              size: 60,
+              color: const Color.fromARGB(255, 255, 0, 0),
+            ),
+
+            SizedBox(height: 20),
+
+            Text(
+              "$contador curtidas",
+              style: TextStyle(
+                fontSize: 18,
+                color: const Color.fromARGB(255, 0, 0, 0),
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+
+            SizedBox(height: 20),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(onPressed: incrementar, child: Text("👍curtir")),
+                SizedBox(width: 10),
+                ElevatedButton(onPressed: excluir, child: Text("👎descurtir")),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
